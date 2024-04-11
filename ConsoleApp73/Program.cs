4a1d3689d8035e547636ef33430563c00cb95423
Michal Budzowski <michal.budzowski@sonarsource.com>
2024-04-11 13:51:27 +0200

    Add project files.

---
+++ ConsoleApp73/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp73
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calling func...");
+            func("");
+        }
+
+
+        int func(String p)
+        {
+            return p.Length;
+        }
+    }
+}
